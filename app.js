9b929318beb782512b0749e66fb39ee724d3f5e9
Pavlo Cherevach <pf4pad.fr@gmail.com>
2022-10-11 12:44:03 +0300

    ToDo_v-model

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ const App = {
     }
   },
   methods: {
-    inputChangeHandler(event) {
-      this.inputValue = event.target.value
-    },
     addNewNote() {
       if (this.inputValue !== '') {
         this.notes.push(this.inputValue)
